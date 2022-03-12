09c848608682e2ad8093ec905f11294b049a4dc5
vickoritz <vickor@attensi.com>
2022-03-12 12:31:49 +0100

    refactor simplify get input field value

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const inputBtn = document.getElementById("input-btn")
 
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(document.getElementById("input-el").value)
+    myLeads.push(inputEl.value)
     console.log(myLeads)
 })
