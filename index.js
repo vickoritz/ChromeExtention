3c1e22806eb6d9fb9c36275872deaf5080e67191
vickoritz <vickor@attensi.com>
2022-03-12 11:45:15 +0100

    Replace onClick with Add EventListener

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-function saveInput() {
-    console.log("Button clicked!")
+
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+    console.log("Button clicked from addEventListener!")
 }
