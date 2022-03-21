e42ff3ad7d7824686d193b5e222f18b2bdec6316
vickoritz <vickor@attensi.com>
2022-03-21 21:29:58 +0100

    convert text in list to links

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ inputBtn.addEventListener("click", function() {
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>"
+        listItems += "<li><a href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
     }
     ulEl.innerHTML = listItems
 }
