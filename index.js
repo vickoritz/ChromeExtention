7253e25bcb16c2d01135704669aece244835ece6
vickoritz <vickor@attensi.com>
2022-03-21 22:57:37 +0100

    bugfix clear myLeads array

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
