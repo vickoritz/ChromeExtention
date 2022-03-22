62c5647300ea61cc6c69543706f9d6a9ce28bf7a
vickoritz <vickor@attensi.com>
2022-03-22 22:25:23 +0100

    save value in localstorage

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-//localStorage.setItem("myLeads", "www.leads.com")
-//console.log(localStorage.getItem("myLeads"))
-//localStorage.clear()
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
 })
 
