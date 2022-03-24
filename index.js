08f5b84891143be6404695d784ac900c685b8ad5
vickoritz <vickor@attensi.com>
2022-03-24 20:47:11 +0100

    get current url from tab

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const inputBtn = document.getElementById("input-btn")
 const deleteBtn = document.getElementById("delete-btn")
 const tabBtn = document.getElementById("tab-btn")
 const ulEl = document.getElementById("ul-el")
-const tabs = [{url: "https://www.linkedin.com/in/per-harald-borgen/"}]
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
 if (leadsFromLocalStorage) {
@@ -43,9 +42,10 @@ inputBtn.addEventListener("click", function() {
 })
 
 
-tabBtn.addEventListener("click", function(){
-    myLeads.push(tabBtn.value)
-    tabBtn.value = ""
-    localStorage.setItem("myLeads", JSON.stringify( myLeads) )
-    render(myLeads)
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
 })
